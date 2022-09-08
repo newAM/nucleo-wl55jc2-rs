f73c51afc0ddad25f60399a80445930130db8eab
Alex Martens <alex@thinglab.org>
2022-09-07 20:31:09 -0700

    rustc compiled

--- flake.nix
+++ flake.nix
@@ -3,18 +3,36 @@
 
   outputs = { self, nixpkgs }:
     let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnsupportedSystem = true;
+        overlays = [
+          (final: prev: {
+            rustc = prev.rustc.overrideAttrs (oA: {
+              RUSTFLAGS = "-Ccodegen-units=32";
+              postConfigure = oA.postConfigure + ''
+                substituteInPlace config.toml \
+                  --replace '#docs = true' 'docs = false'
+              '';
+            });
+          })
+        ];
+      };
       thumbv7emPkgs = import nixpkgs {
+        system = "x86_64-linux";
         crossSystem = nixpkgs.lib.systems.examples.arm-embedded // {
           rustc.config = "thumbv7em-none-eabi";
         };
         config.allowUnsupportedSystem = true;
-        system = "x86_64-linux";
         overlays = [
           (final: prev: {
             rustc = prev.rustc.overrideAttrs (oA: {
+              RUSTFLAGS = "-Ccodegen-units=32";
               postConfigure = oA.postConfigure + ''
+                echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+                exit 1
                 substituteInPlace config.toml \
-                  --replace '#docs = true' 'docs = false`
+                  --replace '#docs = true' 'docs = false'
               '';
             });
           })
@@ -22,6 +40,33 @@
       };
     in
     {
-      packages.x86_64-linux.default = thumbv7emPkgs.callPackage ./package.nix { };
+      packages.x86_64-linux.rustc = pkgs.rustc.override {
+        stdenv = pkgs.stdenv.override {
+          targetPlatform = thumbv7emPkgs.stdenv.targetPlatform;
+          hostPlatform = pkgs.stdenv.hostPlatform;
+          buildPlatform = pkgs.stdenv.buildPlatform;
+          #{ rustc.config = "thumbv7em-none-eabi"; };
+          #targetPlatform = nixpkgs.lib.recursiveUpdate pkgs.multiStdenv.hostPlatform {
+          #  rustc.config = "thumbv7em-none-eabi";
+          #};
+          #targetPlatform = thumbv7emPkgs.stdenv.targetPlatform;
+        };
+        pkgsBuildBuild = pkgs;
+        pkgsBuildHost = pkgs;
+        pkgsBuildTarget.targetPackages.stdenv.cc = pkgs.pkgsCross.arm-embedded.stdenv.cc;
+        #gcc-arm-embedded;
+        #thumbv7emPkgs.targetPackages.stdenv.cc;
+        #= nixpkgs.lib.recursiveUpdate pkgs {
+        #  targetPackages 
+        #};
+#        pkgsBuildTarget = thumbv7emPkgs;
+        enableRustcDev = false;
+      };
+      packages.x86_64-linux.default = pkgs.callPackage ./package.nix {
+        rustPlatform = pkgs.makeRustPlatform {
+          inherit (pkgs.pkgsCross.arm-embedded) rustc;
+          inherit (pkgs) cargo;
+        };
+      };
     };
 }
